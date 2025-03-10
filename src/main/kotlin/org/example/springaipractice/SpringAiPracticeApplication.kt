28d46a5a6399b0f1bdcdc77fad37862e973218f8
hakjun.choi <hakjun.choi@voithru.com>
2025-03-10 10:36:15 +0900

    init

---
+++ src/main/kotlin/org/example/springaipractice/SpringAiPracticeApplication.kt
@@ -0,0 +1,11 @@
+package org.example.springaipractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAiPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/springaipractice/SpringAiPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.springaipractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
